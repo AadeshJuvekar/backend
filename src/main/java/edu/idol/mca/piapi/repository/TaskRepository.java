deadb2d5d13624b1ffad5b5c97b285595675da46
Aadesh Juvekar <juvekaraadesh66@gmai.com>
2023-03-29 09:15:10 +0530

    Updated Backend Application

--- src/main/java/edu/idol/mca/piapi/repository/TaskRepository.java
+++ src/main/java/edu/idol/mca/piapi/repository/TaskRepository.java
@@ -6,6 +6,7 @@ package edu.idol.mca.piapi.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import edu.idol.mca.piapi.domain.Task;
